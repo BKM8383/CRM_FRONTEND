0ea22bbe1057016f8c554df62bf8e4d825d2c335
Bhupendra kumawat <122453339+BKM8383@users.noreply.github.com>
2025-06-03 23:05:36 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.baseURL = 'https://crmxeno.up.railway.app';
 axios.defaults.withCredentials = true;
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 
