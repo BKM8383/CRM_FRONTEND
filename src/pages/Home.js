2c616bca046aee426932d2d0787934b7ea83fe69
Bhupendra kumawat <122453339+BKM8383@users.noreply.github.com>
2025-06-04 16:18:06 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,8 +12,8 @@ const Home = () => {
     (async () => {
       const userInfo = await checkAuth();
       if (!userInfo) {
-        window.location.href = 'https://crmxeno.up.railway.app/oauth2/authorization/google?redirect_uri=${redirectUri}';
-      } else {
+      window.location.href = `https://crmxeno.up.railway.app/oauth2/authorization/google?redirect_uri=${redirectUri}`;
+} else {
         setUser(userInfo);
         setChecking(false);
       }
