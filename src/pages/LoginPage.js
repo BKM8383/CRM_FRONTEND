de0dd6217adfca20a337f6d076084734ff4f0ee6
Bhupendra kumawat <122453339+BKM8383@users.noreply.github.com>
2025-06-04 16:26:09 +0530

    Update LoginPage.js

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 
 const LoginPage = () => {
 useEffect(() => {
-  window.location.href = 'https://crmxeno.up.railway.app/login/oauth2/code/google';
+  window.location.href = 'https://crmxeno.up.railway.app/oauth2/authorization/google';
 }, []);
 
   return <p>Redirecting to Google login...</p>;
